6262ea8c76886901e85148a9a585463c88f8c339
munquasim <munquasim3@gmail.com>
2018-06-09 12:42:31 +0400

    db config changed

--- config/config.js
+++ config/config.js
@@ -4,11 +4,20 @@ module.exports = {
     env: process.env.NODE_ENV || "development",
     port: process.env.PORT || 3000,
     base_url: process.env.BASE_URL || "http://localhost:3000",
+    // db: {
+    //     host: process.env.DB_HOST || 'localhost',
+    //     user: process.env.DB_USER || 'root',
+    //     password: process.env.DB_PSW || '',
+    //     port: process.env.DB_PORT || 3306, //port mysql
+    //     database: process.env.DB_NAME || 'localdb'
+    
+    // },
     db: {
-        host: process.env.DB_HOST || 'localhost',
-        user: process.env.DB_USER || 'root',
-        password: process.env.DB_PSW || '',
+        host: process.env.DB_HOST || 'https://www.db4free.net',
+        user: process.env.DB_USER || 'test_cubezix',
+        password: process.env.DB_PSW || 'test@1234',
         port: process.env.DB_PORT || 3306, //port mysql
-        database: process.env.DB_NAME || 'localdb'
+        database: process.env.MYSQLDB || 'https://www.db4free.net/phpMyAdmin'
+       
     }
 }
